393c324144e067e5ffdacadaeb1c73bd2626da2b
groupe7-ceri <jeremie.decome09@gmail.com>
2014-03-28 13:43:06 +0100

    Premiere version

---
+++ matrice.cpp
@@ -0,0 +1,19 @@
+/* Génie logiciel - Projet Matrice
+L2 INFORMATIQUE - Groupe TP2
+Auteur : Jérémie Décome - Dylan Hernandez - Alexandre Tarbis
+MAJ : 28 mars 2014
+Fichier matrice.cpp
+Implémentation des méthodes de la classe Matrice */
+
+#include "matrice.h"
+
+using namespace std;
+
+Matrice::Matrice()
+{
+	// ctor
+}
+Matrice::~Matrice()
+{
+	// dtor
+}
---
+++ matrice.h
@@ -0,0 +1,13 @@
+/* Génie logiciel - Projet Matrice
+L2 INFORMATIQUE - Groupe TP2
+Auteur : Jérémie Décome - Dylan Hernandez - Alexandre Tarbis
+MAJ : 28 mars 2014
+Fichier matrice.h
+Déclaration des méthodes de la classe Matrice */
+
+#include <iostream>
+
+class Matrice
+{
+public:
+};
