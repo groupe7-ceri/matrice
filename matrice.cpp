37af009f5bf224074b70821104beed1506fa7640
groupe7-ceri <jeremie.decome09@gmail.com>
2014-04-16 23:17:11 +0200

    Remplissage temporaire des trois tableaux en fonction des valeurs du fichier. Attention : le remplissage n'est pas encore terminé !!! Je l'ai mis pour que vous puissiez travailler dessus pendant les vacances. (Pour rappel, je ne serai pas là pendant TOUTES les vacances, donc contact par mail uniquement)
    Pour les méthodes Addition, Soustraction, Multiplication, Division et Factorisation,
    il faudra sans doute remplacer le type de retour par un void (j'en ai parlé avec Dylan)
    
    Alex : participe avec Dylan pour les méthodes citées, cela te permettra d'apprendre le C++ !
    
    N'oubliez pas de faire des commit réguliers (des tutos pour GitHub sur Google)

--- matrice.cpp
+++ matrice.cpp
@@ -1,7 +1,7 @@
 /* Génie logiciel - Projet Matrice
 L2 INFORMATIQUE - Groupe TP2
 Auteur : Jérémie Décome - Dylan Hernandez - Alexandre Tarbis
-MAJ : 11 avril 2014
+MAJ : 16 avril 2014
 Fichier matrice.cpp
 Implémentation des méthodes de la classe Matrice */
 
@@ -12,46 +12,100 @@ using namespace std;
 // Constructeur
 Matrice::Matrice(string nomFichier)
 {
-	cout<<"constructeur avec "<<nomFichier<<endl; // tmp
+	n = 3; // valeurs temporaires
+	m = 3;
+	int tailleFichier = 0; // compteur du nombre de ligne
+	int j = 0;
 	// Lit le fichier
 	const char * nom = nomFichier.c_str(); // conversion string => const char *
 	ifstream fichier(nom);
 	if((fichier != NULL) && (fichier)) // si le fichier existe et non vide
 	{
-		cout<<"Fichier ouvert"<<endl;
-		// lecture de la taille de la matrice
-		//cout<<"Taille : "<<taille<<endl;
+		// lecture de la taille de la matrice (première ligne)
+		string taille;
+		fichier >> taille;
+		// comptage du nombre de ligne
+		string ligne;
+		while(getline(fichier, ligne))
+			tailleFichier++;
 		// Initialisation des trois tableaux
+		// tailleFichier - 1 car on commence à la 2eme ligne
+		tabX = new string[tailleFichier - 1];
+		tabY = new string[tailleFichier - 1];
+		tabVal = new int[tailleFichier - 1];
+		for(int i = 0; i < (tailleFichier - 1); i++)
+		{
+			this->tabX[i] = "x";
+			this->tabY[i] = "y";
+			this->tabVal[i] = 0;
+		}
+		// initialisation du tableau OK
 		// Algo lecture des lignes
+		fichier.clear();
+		fichier.seekg(0, ios::beg); // retour au début du fichier
+		for(int i = 0; i < tailleFichier; i++)
+		{
+			getline(fichier, ligne);
+			if(ligne.find("size=", 0) != 0) // la première ligne est ignorée (car elle contient "size=" à la première occurence
+			{
+				cout<<ligne[0]<<"_"<<ligne[2]<<"_"<<ligne[4]; // tmp
+				//string l = ligne[0];
+				//string c = ligne[2];
+				//this->tabVal[i] = atoi(ligne[4]);
+			}
+			cout<<endl;
+		}
 		// Initialise la matrice
+		this->tabX[0] = "0";
+		this->tabX[1] = "0";
+		this->tabX[2] = "1";
+		this->tabX[3] = "2";
+		this->tabX[4] = "2";
+		this->tabY[0] = "1";
+		this->tabY[1] = "2";
+		this->tabY[2] = "0";
+		this->tabY[3] = "1";
+		this->tabY[4] = "2";
+		this->tabVal[0] = 1;
+		this->tabVal[1] = 2;
+		this->tabVal[2] = 3;
+		this->tabVal[3] = 5;
+		this->tabVal[4] = 7;
 		fichier.close(); // Fermeture du fichier
 	}
 }
 // Méthodes publiques
 Matrice Matrice::addition(Matrice &matrice)
 {
-	return NULL;
 }
 Matrice soustraction(Matrice &matrice)
 {
-	return NULL;
 }
 Matrice multiplifcation(Matrice &matrice)
 {
-	return NULL;
 }
 Matrice division(Matrice &matrice)
 {
-	return NULL;
 }
 Matrice factorisation()
 {
-	return NULL;
 }
 // Méthodes privées
 // Setteurs
+void Matrice::setTabX(string &tabX, int taille)
+{
+	
+}
+void Matrice::setTabY(string &tabY, int taille)
+{
+}
+void Matrice::setTabVal(int &tabVal, int taille)
+{
+}
 // Getteurs
-
+Matrice Matrice::getMatrice()
+{
+}
 // Destructeur
 Matrice::~Matrice()
 {
