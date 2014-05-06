e36d8e7174aeda5b036045619a6b18c2aeac25fd
groupe7-ceri <jeremie.decome09@gmail.com>
2014-05-06 14:54:16 +0200

    gestin.h mise à jour

--- gestion.h
+++ gestion.h
@@ -1,15 +1,26 @@
-/* Génie logiciel - Projet Matrice
-L2 INFORMATIQUE - Groupe TP2
-Auteur : Jérémie Décome - Dylan Hernandez - Alexandre Tarbis
-MAJ : 28 mars 2014
-Fichier gestion.h
-Déclaration des méthodes de la classe Gestion */
+/**
+ * \file gestion.h
+ * \brief Génie logiciel - Projet Matrice - L2 INFORMATIQUE - Groupe TP2
+ * \author Jérémie Décome - Dylan Hernandez - Alexandre Tarbis
+ * \date 28 mars 2014
+ *
+ * \date MAJ 11 Avril 2014
+ *
+ * Déclaration des méthodes de la classe Gestion
+*/
 
 // Bibliothèques / Librairie
 #include <iostream>
 
 // Classes
 #include "matrice.h"
+
+/**
+ * \class Gestion
+ * \brief Classe permettant de gérer les matrices
+ *
+ * Cette classe permet de gérer les matrices
+*/
 
 class Gestion
 {
