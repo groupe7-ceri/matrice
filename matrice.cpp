92a4a33958b493ba56d97892d544d1160878a4db
groupe7-ceri <jeremie.decome09@gmail.com>
2014-05-07 14:04:18 +0200

    Ajout constructeur de recopie

--- matrice.cpp
+++ matrice.cpp
@@ -1,89 +1,65 @@
-<<<<<<< HEAD
 /* Génie logiciel - Projet Matrice
 L2 INFORMATIQUE - Groupe TP2
 Auteur : Jérémie Décome - Dylan Hernandez - Alexandre Tarbis
 MAJ : 6 mai 2014
 Fichier matrice.cpp
 Implémentation des méthodes de la classe Matrice */
-=======
-/**
- * \file matrice.cpp
- * \brief Génie logiciel - Projet Matrice - L2 INFORMATIQUE - Groupe TP2
- * \author Jérémie Décome - Dylan Hernandez - Alexandre Tarbis
- * \date 28 mars 2014
-  *
- * \date MAJ 11 Avril 2014
- *
- * Implémentation des méthodes de la classe Matrice
-*/
->>>>>>> 075c882961b5cc2e68b881959bf9153509d16d43
 
 #include "matrice.h"
 
-/** \namespace std */
-
 using namespace std;
 
-/**
- * \fn Matrice::Matrice (string nomFichier)
- * \brief Constructeur
- *
- * Ce constructeur appele un fichier.txt et récupère les données qu'il contient
- * afin de créer une matrice composer de trois tableau
- * Le premier tableau tabX contient le numéro de la colonne de la matrice
- * Le deuxième tableau tabY contient le numéro de la ligne de la matrice
- * Le troisième tableau tabVal contient le valeur correspondant à la case de la matrice définie par les
- * deux premiers tableaux
- *
- * \param nomFichier  Nom du fichier que le constructeur doit ouvrir
- *
- */
+// Constructeur
 Matrice::Matrice(string nomFichier)
 {
-<<<<<<< HEAD
-=======
-	n = 3; /** Valeurs temporaires */
-	m = 3;
-	int tailleFichier = 0; // compteur du nombre de ligne
->>>>>>> 075c882961b5cc2e68b881959bf9153509d16d43
 	int j = 0;
+	int separateur = 6; // position du séparateur (par défaut, il est sur le caractère 6)
+	int c1 = -1, c2 = -1;
 	// initialisation des variables
 	this->tailleFichier = 0;
+	this->n = -1;
+	this->m = -1;
+	this->max = -1;
 	// Lit le fichier
 	const char * nom = nomFichier.c_str(); // conversion string => const char *
-	ifstream fichier(nom);
+	ifstream fichier(nom); // ouverture du fichier
 	if((fichier != NULL) && (fichier)) // si le fichier existe et non vide
 	{
 		// lecture de la taille de la matrice (première ligne)
 		string taille;
 		fichier >> taille; // extraction de la première ligne
 		// traitement de la première ligne (n et m)
-		//cout<<"n : "<<taille[5]<<" - m : "<<taille[7]<<endl; // tmp
-		cout<<"taille : "<<taille.size()<<endl;
-		if(taille.size() > 8)
+		// lecture de n
+		c1 = this->charToInt(taille[5]);
+		if(taille[6] != '-') // si la lettre 6 n'est pas un séparateur
 		{
-			cout<<"n ou m est à 2 chiffres"<<endl;
-			// algo a faire
+			c2 = this->charToInt(taille[6]);
+			c1 *= 10; // concatène c1 et c2
+			c1 += c2;
+			separateur = 7; // séparateur sur le caractère 7
 		}
-		else // la valeur de n et de m contient un seul chiffre
+		this->n = c1; // stockage de la valeur définitive c1 de n
+		// lecture de m
+		c1 = this->charToInt(taille[separateur + 1]); // réassignation de c1 et de c2
+		if(taille[separateur + 2] != '\0') // si la case existe, c'est qu'il y a un chiffre
 		{
-			this->n = this->charToInt(taille[5]);
-			this->m = this->charToInt(taille[7]);
+			c2 = this->charToInt(taille[separateur + 2]);
+			c1 *= 10; // concatène c1 et c2
+			c1 += c2;
 		}
-		cout<<"n : "<<this->n<<" - m : "<<this->m<<endl; // tmp
-		this->max = this->n * this->m; // calcul du nombre de valeurs maximal
+		this->m = c1;  // stockage de la valeur définitive c1 de m
+		this->max = this->n * this->m; // calcul du nombre de valeurs maximale
 		// comptage du nombre de ligne
 		string ligne;
 		while(getline(fichier, ligne))
 			this->tailleFichier++;
-		if(this->tailleFichier ==	 0) 
+		if(this->tailleFichier == 0) 
 		{
 			cout<<"Erreur de lecture du fichier, arret du programme"<<endl;
 			exit(4); // arret du programme
 		}
 		// Initialisation des trois tableaux
-		// tailleFichier - 1 car on commence à la 2eme ligne
-		tabX = new string[tailleFichier - 1];
+		tabX = new string[tailleFichier - 1]; // tailleFichier - 1 car on commence à la 2eme ligne
 		tabY = new string[tailleFichier - 1];
 		tabVal = new int[tailleFichier - 1];
 		for(int i = 0; i < (tailleFichier - 1); i++)
@@ -92,168 +68,64 @@ Matrice::Matrice(string nomFichier)
 			this->tabY[i] = "y";
 			this->tabVal[i] = 0;
 		}
-		// Algo lecture des lignes et stockage dans les trois tableaux
-		fichier.clear();
-		fichier.seekg(0, ios::beg); // retour au début du fichier
+		fichier.clear(); // retour au début du fichier
+		fichier.seekg(0, ios::beg);
+		// Lecture des lignes et stockage dans les trois tableaux
 		int i = 0;
 		while(getline(fichier, ligne))
 		{
 			if(ligne.find("size=", 0) != 0) // la 1e ligne (contenant size=) n'est pas lu
 			{
-				//cout<<"ligne "<<i<<" : "<<ligne<<endl;
 				char l = ligne[0];
 				this->tabX[i] = l;
 				char c = ligne[2];
 				this->tabY[i] = c;
 				this->tabVal[i] = this->charToInt(ligne[4]);
-				cout<<i<<" : "<<this->tabX[i]<<"_"<<this->tabY[i]<<"_"<<this->tabVal[i]<<endl; // tmp, pour débug
 				i++;
 			}
 			
 		}
 		fichier.close(); // Fermeture du fichier
 	}
+	// récapitulatif
+	cout<<"Matrice créée :"<<endl;
+	cout<<" - n : "<<this->n<<endl;
+	cout<<" - m : "<<this->m<<endl;
+	if(this->creuse())
+		cout<<"Matrice creuse"<<endl;
+	else
+		cout<<"Matrice pleine"<<endl;
+}
+Matrice::Matrice(Matrice &matrice) // constructeur de recopie
+{
+	this->n = matrice.n;
+	this->m = matrice.m;
+	this->max = matrice.max;
+	this->tailleFichier = matrice.tailleFichier;
+	// Initialisation des trois tableaux
+	tabX = new string[tailleFichier - 1]; // tailleFichier - 1 car on commence à la 2eme ligne
+	tabY = new string[tailleFichier - 1];
+	tabVal = new int[tailleFichier - 1];
+	for(int i = 0; i < (tailleFichier - 1); i++)
+	{
+		this->tabX[i] = "x";
+		this->tabY[i] = "y";
+		this->tabVal[i] = 0;
+	}
+	// recopie tabX
+	for(int i = 0; i < this->tailleFichier; i++)
+	{
+		this->tabX[i] = matrice.tabX[i];
+		this->tabY[i] = matrice.tabY[i];
+		this->tabVal[i] = matrice.tabVal[i];
+	}
+	// recopie tabY
+	// recopie tabVal
 }
 // Méthodes publiques
-<<<<<<< HEAD
-void Matrice::addition(Matrice &matrice)
-=======
-
-/**
- * \fn Matrice::addition (Matrice &matrice)
- * \brief Addition
- *
- * Fonction permettant l'addition de 2 matrices
- *
- * \param Une matrice
- *
- */
-
 void Matrice::addition(Matrice &matrice)
 {
-    int i=0;
-    while (i < (tailleFichier - 1))
-    {
-        if(tabX[i] == matrice->tabX[i] && tabY[i] == matrice->tabY[i])
-        {
-            if(tabVal[i] == 0)
-            {
-                if(matrice->tabVal[i] == 0) i++;
-                else
-                {
-                    tabVal[i] = matrice->tabVal[i];
-                    i++
-                }
-            }
-            else
-            {
-                if(matrice->tabVal[i] == 0) i++;
-                else
-                {
-                    tabVal[i] = tabVal[i] + matrice->tabVal[i];
-                    i++;
-                }
-            }
-        }
-        else
-        {
-			if(tabX[i] == matrice->tabX[i])
-			{
-				if(tabY[i] < matrice->tabY[i])
-				{
-					insertion(tabX[i],tabY[i],tabVal[i],i);
-					i++;
-				}
-				else
-				{
-					insertion(tabX[i],tabY[i],tabVal[i],i+1);
-					i++;
-				}
-			}
-			else if(tabX[i] < matrice->tabX[i])
-			{
-				insertion(tabX[i],tabY[i],tabVal[i],i);
-				i++;
-			}
-			else if(tabX[i] < matrice->tabX[i])
-			{
-				insertion(tabX[i],tabY[i],tabVal[i],i+1);
-				i++;
-			}
-        }
-    }
 }
-
-/**
- * \fn Matrice::soustraction (Matrice &matrice)
- * \brief Soustraction
- *
- * Fonction permettant la soustraction de 2 matrices
- *
- * \param Une matrice
- *
- */
-
-void Matrice::soustraction(Matrice &matrice)
->>>>>>> 075c882961b5cc2e68b881959bf9153509d16d43
-{
-    int i=0;
-    while (i < (tailleFichier - 1))
-    {
-        if(tabX[i] == matrice->tabX[i] && tabY[i] == matrice->tabY[i])
-        {
-            if(tabVal[i] == 0)
-            {
-                if(matrice->tabVal[i] == 0) i++;
-                else
-                {
-                    tabVal[i] = tabVal[i] - matrice->tabVal[i];
-                    i++
-                }
-            }
-            else
-            {
-                if(matrice->tabVal[i] == 0) i++;
-                else
-                {
-                    tabVal[i] = tabVal[i] - matrice->tabVal[i];
-                    i++;
-                }
-            }
-        }
-        else
-        {
-			if(tabX[i] == matrice->tabX[i])
-			{
-				if(tabY[i] < matrice->tabY[i])
-				{
-                    tabVal[i] = tabVal[i] - matrice->tabVal[i];
-					insertion(tabX[i],tabY[i],tabVal[i],i);
-					i++;
-				}
-				else
-				{
-                    tabVal[i] = tabVal[i] - matrice->tabVal[i];
-					insertion(tabX,tabY,tabVal,i+1);
-					i++;
-				}
-			}
-			else if(tabX[i] < matrice->tabX[i])
-			{
-                tabVal[i] = tabVal[i] - matrice->tabVal[i];
-				insertion(tabX,tabY,tabVal,i);
-				i++;
-			}
-			else if(tabX[i] < matrice->tabX[i])
-			{
-                tabVal[i] = tabVal[i] - matrice->tabVal[i];
-				insertion(tabX,tabY,tabVal,i+1);
-				i++;
-			}
-        }
-    }
-}
-<<<<<<< HEAD
 void Matrice::soustraction(Matrice &matrice)
 {
 }
@@ -264,48 +136,21 @@ void Matrice::division(Matrice &matrice)
 {
 }
 void Matrice::factorisation()
-=======
-
-void Matrice::multiplication(Matrice &matrice)
 {
-}
-
-void Matrice::division(Matrice &matrice)
-{
-}
-
-void Matrice::factorisation()
-{
-}
-
-/**
- * \fn Matrice::Insretion (string *tab1,string *tab2,int *tab3,int i)
- * \brief Insretion
- *
- * Fonction permettant la soustraction de 2 matrices
- *
- * \param 2 chaîne de caractère et duex entiers
- *
- */
-Matrice Insertion(string s1,string s2,int s3,int i)
->>>>>>> 075c882961b5cc2e68b881959bf9153509d16d43
-{
-
-
 }
 void Matrice::afficher()
 {
-	cout<<"Voici la matrice :"<<endl;
 	string x, y;
 	int val;
+	cout<<"Voici la matrice :"<<endl;
 	for(int i = 0; i < (this->tailleFichier - 1); i++)
 	{
 		x = tabX[i]; // récupération de la valeur X
 		y = tabY[i]; // récupération de la valeur Y
 		val = tabVal[i]; // récupération de la valeur
-		cout<<"i : "<<i<<" - ["<<x<<"]["<<y<<"] val : "<<val<<endl;
+		cout<<"i : "<<i<<" - ["<<x<<"]["<<y<<"] val : "<<val<<endl; // affichage temporaire
 	}
-	
+	// affichage sous forme d'une matrice
 }
 // Méthodes privées
 int Matrice::charToInt(char caractere)
@@ -320,10 +165,20 @@ int Matrice::charToInt(char * caractere)
 	valeur = atoi(caractere);
 	return valeur;
 }
+bool Matrice::creuse() // détermine si la matrice est creuse ou non
+{
+	bool creuse = false;
+	int nbre = this->tailleFichier; // nombre de cases occupés
+	float pc = 50 / 100;
+	cout<<"max : "<<this->max<<" / nb : "<<nbre<<" = ";
+	printf("pc : %.2f \n", pc); // instruction C pour affichage
+	// calcul du nombre de case occupé par colonne (il faut au moins 1 case / colonne)
+	return creuse;
+}
 // Setteurs
 void Matrice::setTabX(string &tabX, int taille)
 {
-
+	
 }
 void Matrice::setTabY(string &tabY, int taille)
 {
@@ -335,6 +190,17 @@ void Matrice::setTabVal(int &tabVal, int taille)
 Matrice Matrice::getMatrice()
 {
 	
+}
+int Matrice::getMax()
+{
+	if(this->max > 0)
+		return this->max;
+	else
+		return 0;
+}
+int Matrice::getTaille()
+{
+	return this->tailleFichier;
 }
 // Destructeur
 Matrice::~Matrice()
